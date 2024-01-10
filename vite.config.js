2a84012ba1d9994df8ca2804161b5a3443687d2d
AvitBrian <80854528+leudemon@users.noreply.github.com>
2024-01-10 15:36:48 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
+const ASSET_URL = process.env.ASSET_URL || '';
+
 export default defineConfig({
   plugins: [react()],
+  base: `${ASSET_URL}/dist/`,
 })
